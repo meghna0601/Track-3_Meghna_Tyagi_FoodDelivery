c66908ccf2ee1f2df5d383a4bcaec1469c484e01
meghtyagi <meghtyagi@deloitte.com>
2026-02-12 13:27:38 +0530

    add swagger & actuator also added user class

---
+++ src/main/java/com/fooddelivery/Role.java
@@ -0,0 +1,10 @@
+package com.fooddelivery;
+
+public enum Role {
+
+    CUSTOMER,
+    DRIVER,
+    RESTAURANT,
+    ADMIN
+
+}
---
+++ src/main/java/com/fooddelivery/model/User.java
@@ -0,0 +1,24 @@
+package com.fooddelivery.model;
+
+import com.fooddelivery.Role;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@Builder(toBuilder = true)
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String address;
+    private Boolean active;
+    private String phoneNumber;
+    private Role role;
+
+}
