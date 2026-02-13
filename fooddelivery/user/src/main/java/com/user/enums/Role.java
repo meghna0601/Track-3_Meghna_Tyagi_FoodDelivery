2a0e5f6d17a136394035807da5f2734bb6aabd8d
meghtyagi <meghtyagi@deloitte.com>
2026-02-13 17:45:12 +0530

    change springboot to microservices architecture

---
+++ fooddelivery/user/src/main/java/com/user/enums/Role.java
@@ -0,0 +1,9 @@
+package com.user.enums;
+
+public enum Role {
+    CUSTOMER,
+    ADMIN,
+    DRIVER,
+    RESTAURANT;
+
+}
---
+++ fooddelivery/user/src/main/java/com/user/model/User.java
@@ -0,0 +1,22 @@
+package com.user.model;
+
+import com.user.enums.Role;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@Builder(toBuilder = true)
+public class User {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String address;
+    private Boolean active;
+    private String phoneNumber;
+    private Role role;
+}
