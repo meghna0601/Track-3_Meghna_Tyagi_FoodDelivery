87e389e3708110ef287cb70788e735d16dd1777f
meghtyagi <meghtyagi@deloitte.com>
2026-02-16 15:13:54 +0530

    few more apis for user

--- fooddelivery/api-gateway/src/main/java/com/api_gateway/ApiGatewayApplication.java
+++ fooddelivery/api-gateway/src/main/java/com/api_gateway/ApiGatewayApplication.java
@@ -1,14 +1,14 @@
-package com.user;
+package com.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class UserApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
